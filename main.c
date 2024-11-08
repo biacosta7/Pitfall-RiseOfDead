c6dab34edfd83b7f68fb8c7ff86eea0a13bf70ec
biacosta7 <contact.beatrizcosta@gmail.com>
2024-11-08 20:22:46 -0300

    pulo do player

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 #include <math.h>
 
 #define SCALE_FACTOR 1.6
-#define SCREEN_HEIGHT 900
-#define SCREEN_WIDTH 40
+#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 800
 
 // struct player
 typedef struct{
     Vector2 position;
     Vector2 size;
     Color color;
+    float velocityY; // velocidade vertical (para controlar o pulo)
+    bool isJumping;
 } Player;
 
 // struct enemy
@@ -18,6 +20,7 @@ typedef struct{
     Vector2 position;
     Vector2 size;
     Color color;
+    float velocityY;
 } Enemy;
 
 // draw player e enemy
@@ -25,11 +28,35 @@ void DrawPlayer(Player player){
     DrawRectangleV(player.position, player.size, player.color);
 }
 
+void aplica_gravidade(Player *player, Enemy *enemy) {
+    float gravidade = 0.5f;
+    player->velocityY += gravidade; // acumula a gravidade na velocidade
+    
+    player->position.y += player->velocityY; // atualiza a posição com base na velocidade
+    
+    // verifica se o player atingiu o chão
+    float groundLevel = (SCREEN_HEIGHT) - player->size.y;
+    if (player->position.y > groundLevel) {
+        player->position.y = groundLevel;
+        player->velocityY = 0; // para a velocidade quando o player chega no chão
+        player->isJumping = false; 
+    }
+
+    enemy->velocityY += gravidade;
+    enemy->position.y += enemy->velocityY;
+
+    if (enemy->position.y > groundLevel) {
+        enemy->position.y = groundLevel;
+        enemy->velocityY = 0;
+    }
+}
+
+
 int main(void){
     // cria window
-    int screenWidth = 800 * SCALE_FACTOR;
-    int screenHeight = 600 * SCALE_FACTOR;
-    InitWindow(screenWidth, screenHeight, "Raylib Game");
+    int screenWidth = SCREEN_WIDTH * SCALE_FACTOR;
+    int screenHeight = SCREEN_HEIGHT * SCALE_FACTOR;
+    InitWindow(screenWidth, screenHeight, "Pitfall - Rise Of Dead");
 
     // cria player
     Player player = {0};
@@ -47,8 +74,9 @@ int main(void){
     while (!WindowShouldClose())
     {
         // movimento player
-        if (IsKeyDown(KEY_W)){
-            player.position.y -= 1;
+        if (IsKeyPressed(KEY_W) && !player.isJumping) {
+            player.velocityY = -10.0f; // velocidade de pulo inicial
+            player.isJumping = true;
         }
         if (IsKeyDown(KEY_A)){
             player.position.x -= 1;
@@ -69,6 +97,8 @@ int main(void){
         } else if(player.position.y < enemy.position.y){
             enemy.position.y -= 0.3;
         }
+
+        aplica_gravidade(&player, &enemy);
 
         // draw the game
         BeginDrawing();
